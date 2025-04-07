ea3a6b8175073faf413c442ef1ddb14aa73b9978
MarcoShvy <mass7813@gmail.com>
2025-04-07 18:02:22 -0300

    up

--- src/main/java/com/aceleramaker/blog/repository/UserRepository.java
+++ src/main/java/com/aceleramaker/blog/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.aceleramaker.blog.repository;
 
 import com.aceleramaker.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
